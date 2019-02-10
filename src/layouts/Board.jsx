d6e75bf0522d378f966905fd3b6e95548bd55305
alexengrig <alexengrigdev@gmail.com>
2019-02-10 18:55:49 +0300

    Create layouts Main and Board

---
+++ src/layouts/Board.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+  render() {
+    return (
+      <div>
+        <h1>BOARD</h1>
+        <main>
+          content
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>MAIN</h1>
+        <main>
+          content
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Main;
