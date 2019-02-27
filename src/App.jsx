5e27a0c5a4b5b59b57d314941eaf41cf57c8b5d4
alekseenko <alexengrigdev@gmail.com>
2019-02-27 18:28:25 +0300

    Add redux
    Add dependency redux
    Add dependency react-redux
    Create function for configure store
    Create simple reducer
    Add store provider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React, { Component } from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
-import routes from "./routes";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import routes from './routes';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <Switch>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.pathname}
-              component={route.component}
-            />
-          ))}
-        </Switch>
-      </HashRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.pathname}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
