5a5960fc2e34057a17b154cc213a831aeb57e475
alexengrig <alexengrigdev@gmail.com>
2019-03-17 01:17:40 +0300

    Add link to home button

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { AppBar, IconButton, Toolbar, withStyles } from '@material-ui/core';
 import { Dashboard as DashboardIcon, Home as HomeIcon } from '@material-ui/icons';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { mainRoutes as routes } from '../../routes';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { main as mainRoute, mainRoutes as routes } from '../../routes';
 
 const styles = theme => ({
   root: {
@@ -25,7 +25,7 @@ class Main extends Component {
       <div className={classes.root}>
         <AppBar>
           <Toolbar className={classes.toolbar} variant='dense'>
-            <IconButton color='inherit'>
+            <IconButton color='inherit' component={NavLink} to={mainRoute.path}>
               <HomeIcon />
             </IconButton>
             <IconButton color='inherit'>
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const routes = [admin, main];
 
 export default routes;
 
+export { admin, main };
 export { subroutes as adminRoutes } from './admin';
 export { subroutes as mainRoutes } from './main';
