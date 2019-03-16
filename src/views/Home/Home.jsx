bfe2cc6e3aa352a40b24ff070458b668a44bfb00
alexengrig <alexengrigdev@gmail.com>
2019-03-16 23:26:06 +0300

    Create Home, Board and Team views

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          Nav
+        </section>
+        <section>
+          Content
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
